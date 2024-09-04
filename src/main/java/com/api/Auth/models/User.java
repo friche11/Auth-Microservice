eb779da626971f91517ba9fa91fda9e4252e5ee9
friche11 <vicfriche@gmail.com>
2024-09-04 18:38:40 -0300

    feat: criação do model de usuário

---
+++ src/main/java/com/api/Auth/models/User.java
@@ -0,0 +1,85 @@
+package com.api.Auth.models;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonProperty.Access;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+@Entity
+@Table(name = "user")
+public class User {
+
+
+    @Id
+    @Column(name = "id", unique = true)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "username", length = 100, nullable = false, unique = true)
+    @Size(min = 2, max = 100)
+    @NotBlank
+    private String username;
+
+    @Column(name = "email", length = 256, nullable = false, unique = true)
+    @Size(min = 5, max = 100)
+    @NotBlank
+    @Email(message = "Email deve ser válido")
+    private String email;
+
+    @JsonProperty(access = Access.WRITE_ONLY)
+    @Column(name = "password", length = 60, nullable = false)
+    @Size(min = 8, max = 60)
+    @NotBlank
+    private String password;
+
+    public User() {
+    }
+
+    public User(Long id, String username, String email, String password) {
+        this.id = id;
+        this.username = username;
+        this.email = email;
+        this.password = password;
+    }
+
+    public Long getId() {
+        return this.id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return this.username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return this.email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return this.password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    }
