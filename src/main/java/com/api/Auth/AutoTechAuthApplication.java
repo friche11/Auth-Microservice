5d59a2fa46c375c42f8fd08afd9b33b5dccd873b
friche11 <vicfriche@gmail.com>
2024-09-04 16:54:06 -0300

    Iniciando projeto

---
+++ src/main/java/com/api/Auth/AutoTechAuthApplication.java
@@ -0,0 +1,13 @@
+package com.api.Auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutoTechAuthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutoTechAuthApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/api/Auth/AutoTechAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.Auth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutoTechAuthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
